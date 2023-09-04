accbdcc116b956333fbb224faf2d0847210a791d
BANU PRIYA D <139879880+priyad1221@users.noreply.github.com>
2023-09-04 18:41:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ total_tip_amount=bill*tip_as_percent
 total_bill=bill + total_tip_amount
 bill_per_person=total_bill/people
 final_amount="{:.2f}".format(bill_per_person)
-print(f"Each person should pay {final_amount}")500
+print(f"Each person should pay {final_amount}")
